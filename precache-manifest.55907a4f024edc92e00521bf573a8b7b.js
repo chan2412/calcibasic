a3c98a9c8efda57b7b6ec253315dcf3fca808a87
CHANDHRU R <44549194+chan2412@users.noreply.github.com>
2020-04-23 13:10:27 +0530

    Updates

---
+++ precache-manifest.55907a4f024edc92e00521bf573a8b7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd669b33c2da9e075fe8260c17527e8d",
+    "url": "/calcibasic/index.html"
+  },
+  {
+    "revision": "33eb336b37d39fa1620c",
+    "url": "/calcibasic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5782aff738369de6a7d9",
+    "url": "/calcibasic/static/js/2.e7030bd9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/calcibasic/static/js/2.e7030bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33eb336b37d39fa1620c",
+    "url": "/calcibasic/static/js/main.b6cf19f6.chunk.js"
+  },
+  {
+    "revision": "4ece681e136f334c970e",
+    "url": "/calcibasic/static/js/runtime-main.c2b19896.js"
+  }
+]);
